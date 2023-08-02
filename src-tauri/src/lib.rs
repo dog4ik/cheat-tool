31114ce251a124c917c10cc777dc66cd774b2dde
Vadim <angryvadik@gmail.com>
2023-08-02 21:43:29 +0200

    even shittier keypress watcher

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,5 +5,7 @@
 #![feature(array_windows)]
 pub mod emit_keypress;
 pub mod process;
+pub mod watch_keypress;
 pub use process::Process;
 pub use process::ProcessError;
+pub use watch_keypress::get_key_press;
---
+++ src-tauri/src/watch_keypress.rs
@@ -0,0 +1,70 @@
+use std::{fmt::Display, process::Stdio};
+
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::Command;
+use tokio::sync::mpsc::{channel, Receiver};
+
+#[derive(Debug, Clone, Copy)]
+pub enum ParseError {
+    UnknownError,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::UnknownError => write!(f, "Unknown error occured"),
+        }
+    }
+}
+
+fn parse_code(line: &str) -> Result<bool, ParseError> {
+    let code: u32 = line
+        .split_whitespace()
+        .into_iter()
+        .skip(2)
+        .next()
+        .ok_or_else(|| ParseError::UnknownError)?
+        .trim()
+        .parse()
+        .map_err(|_| ParseError::UnknownError)?;
+
+    if code == 2 {
+        return Ok(true);
+    } else if code == 3 {
+        return Ok(false);
+    } else {
+        return Err(ParseError::UnknownError);
+    }
+}
+
+pub async fn get_key_press(key_code: u32) -> Receiver<bool> {
+    let (sender, receiver) = channel(100);
+    tokio::spawn(async move {
+        let mut is_pressed = false;
+        let child = Command::new("xinput")
+            .args(["test-xi2", "--root"])
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("failed to execute process");
+        if let Some(stdout) = child.stdout {
+            let reader = BufReader::new(stdout);
+            let mut lines = reader.lines();
+            while let Some(line) = lines.next_line().await.unwrap() {
+                if let Some((key, value)) = line.split_once(":") {
+                    if key.trim() == "detail" && value.trim().parse::<u32>().unwrap() == key_code {
+                        sender.send(is_pressed).await.unwrap();
+                    }
+                } else {
+                    if let Ok(val) = parse_code(line.trim()) {
+                        is_pressed = val;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+    });
+    //we are not returning receiver
+    println!("return");
+    return receiver;
+}
